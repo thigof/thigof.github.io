2355561d3845cae92663872eda1b8ec849d71891
Marco Antônio <marco.aq7@gmail.com>
2024-11-01 20:08:00 -0300

    up

--- project/src/pages/IndexPage.vue
+++ project/src/pages/IndexPage.vue
@@ -13,32 +13,32 @@
         <b class="text-red"> {{ (computedNpat) ? "" : "Não encontrado" }}</b>
       </label>
       <div class="row">
-        <input @:change="handleNpat" v-model="form.npat" type="text" style="width: auto;" placeholder="Digite NPAT" />
+        <input @:change="handleNpat" v-model="form.npat" type="text" style="width: auto;" />
         <button @click="handleNpat" :disabled="!computedNpat">Pesquisar</button>
       </div>
       <label>
         <input type="checkbox" v-model="form.descricaoChecked" />
         Descrição
       </label>
-      <input v-model="form.descricao" type="text" class="input-flex" placeholder="Digite Descrição" />
+      <input v-model="form.descricao" type="text" class="input-flex" />
 
       <label>
         <input type="checkbox" v-model="form.localChecked" />
         Local
       </label>
-      <input v-model="form.local" type="text" class="input-flex" placeholder="Digite Local" />
+      <input v-model="form.local" type="text" class="input-flex" />
 
       <label>
         <input type="checkbox" v-model="form.estadoChecked" />
         Estado
       </label>
-      <input v-model="form.estado" type="text" class="input-flex" placeholder="Digite Estado" />
+      <input v-model="form.estado" type="text" class="input-flex" />
 
       <label>
         <input type="checkbox" v-model="form.obsChecked" />
         Observação
       </label>
-      <input v-model="form.obs" type="text" class="input-flex" placeholder="Digite Observação" />
+      <input v-model="form.obs" type="text" class="input-flex" />
 
       <button type="button" @click="gerarCSV">Gerar CSV</button>
       <button type="button" @click="gerarHTML">Gerar HTML</button>
@@ -168,98 +168,3 @@ onMounted(() => {
   }
 });
 </script>
-
-<style scoped>
-form {
-  background-color: #ffffff;
-  padding: 20px;
-  margin: 5px auto;
-  border-radius: 10px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-  max-width: 600px;
-  width: 100%;
-}
-
-form label {
-  margin-bottom: 8px;
-  font-family: Arial, Helvetica, sans-serif;
-  font-size: 14px;
-  color: #3a3a3a;
-}
-
-form input[type="text"] {
-  display: flex;
-  width: 100%;
-  padding: 8px;
-  margin-bottom: 1px;
-  border-radius: 5px;
-  border: 1px solid #ccc;
-  box-sizing: border-box;
-  font-size: 14px;
-}
-
-form button {
-  background-color: #4caf50;
-  color: white;
-  padding: 8px 16px;
-  border: none;
-  border-radius: 5px;
-  font-size: 14px;
-  font-family: Arial, Helvetica, sans-serif;
-  cursor: pointer;
-}
-
-table {
-  border-collapse: collapse;
-  width: 100%;
-  margin-top: 20px;
-  font-family: Arial, Helvetica, sans-serif;
-  font-size: 14px;
-}
-
-th,
-td {
-  padding: 8px;
-  text-align: left;
-  border-bottom: 1px solid #ddd;
-  font-size: 14px;
-}
-
-th {
-  background-color: #ffffff;
-  border-bottom: 1px;
-  background-color: rgb(245, 245, 245);
-  font-size: 14px;
-}
-
-.erro {
-  color: red;
-  font-size: 14px;
-  font-family: Arial, Helvetica, sans-serif;
-}
-
-.inline {
-  display: inline-block;
-  vertical-align: middle;
-}
-
-fieldset {
-  display: block;
-  width: 100%;
-  padding: 8px;
-  margin-bottom: 5px;
-  border-radius: 5px;
-  border: 1px solid #ccc;
-  box-sizing: border-box;
-  font-size: 14px;
-}
-
-.inline-block {
-  display: inline-block;
-  vertical-align: middle;
-}
-
-.form-controls {
-  margin-top: 10px;
-}
-</style>
