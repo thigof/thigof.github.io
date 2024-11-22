24b01d93aedf27c3f6c99c7a08f5d270a269f490
Marco Antônio <marco.aq7@gmail.com>
2024-11-22 13:39:50 -0300

    UpApp

--- project/src/layouts/MainLayout.vue
+++ project/src/layouts/MainLayout.vue
@@ -1,28 +1,26 @@
 <template>
   <q-layout view="lHh lpr lFf">
-    <q-header>
-      <q-btn
-        color="primary"
-        flat
-        round
-        icon="more_vert"
-        class="print-hide z-top absolute-top-right q-pa-md"
-      >
-        <q-menu anchor="top right" self="top end" class="z-top">
-          <q-list>
-            <q-item to="/" clickable v-close-popup>
-              <q-item-section>Principal</q-item-section>
-            </q-item>
-            <q-item to="/settings" clickable v-close-popup>
-              <q-item-section>Configurações</q-item-section>
-            </q-item>
-            <q-item @click="restore" clickable v-close-popup>
-              <q-item-section>Restaurar</q-item-section>
-            </q-item>
-          </q-list>
-        </q-menu>
-      </q-btn>
-    </q-header>
+    <q-btn
+      color="primary"
+      flat
+      round
+      icon="more_vert"
+      class="z-top absolute-top-right q-pa-md"
+    >
+      <q-menu anchor="top right" self="top end" class="z-top">
+        <q-list>
+          <q-item to="/" clickable v-close-popup>
+            <q-item-section>Principal</q-item-section>
+          </q-item>
+          <q-item to="/settings" clickable v-close-popup>
+            <q-item-section>Configurações</q-item-section>
+          </q-item>
+          <q-item @click="restore" clickable v-close-popup>
+            <q-item-section>Restaurar</q-item-section>
+          </q-item>
+        </q-list>
+      </q-menu>
+    </q-btn>
     <q-page-container style="padding: 3px 4px 3px 3px">
       <router-view />
     </q-page-container>
--- project/src/pages/SettingsPage.vue
+++ project/src/pages/SettingsPage.vue
@@ -9,13 +9,27 @@
       />
     </fieldset>
   </div>
+  <ShowTables
+    :tableData="app.values"
+    :headers="colunas"
+    @remove="handleRemoveTable"
+    @clicked="handleClickedTable"
+  />
 </template>
 
 <script setup>
-import { handleFileUpload } from "src/boot/app";
+import { app, handleFileUpload } from "src/boot/app";
+import ShowTables from "src/components/ShowTables.vue";
+import { computed } from "vue";
 import { useRouter } from "vue-router";
 
 const router = useRouter();
+
+const colunas = computed(() => {
+  return Object.keys(app.values[0])
+    .filter((e) => !["UPDATED", "OBSERVAÇÃO", "SITUAÇÃO", "id"].includes(e))
+    .map((e) => ({ field: e, label: e }));
+});
 
 const handleFile = (event) => {
   handleFileUpload(event);
