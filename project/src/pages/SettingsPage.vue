2f5ddec8aebc4659e9da6c053eaf319cc7c278f8
Marco Antônio <marco.aq7@gmail.com>
2024-11-22 14:43:27 -0300

    UpApp

--- project/src/pages/SettingsPage.vue
+++ project/src/pages/SettingsPage.vue
@@ -23,7 +23,7 @@
       v-model="data.multiple"
       :options="['10', '50', '100', '1000', '10000']"
       label="Limite"
-      style="width: 250px"
+      style="width: 100px"
     />
   </div>
 
